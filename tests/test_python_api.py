66d4caeaca43a977150aa33141a6fce1b3e244ca
autopep8 <autopep8@users.noreply.github.com>
2021-10-01 18:16:17 +0000

    Automated autopep8 fixes

--- tests/test_python_api.py
+++ tests/test_python_api.py
@@ -19,7 +19,7 @@ class TestApiUsage(unittest.TestCase):
 
         returned_filename = stl_to_h5m(
             files_with_tags=[
-                    ("tests/part2.stl", "mat1")
+                ("tests/part2.stl", "mat1")
             ],
             h5m_filename=test_h5m_filename,
         )
@@ -29,7 +29,8 @@ class TestApiUsage(unittest.TestCase):
         assert test_h5m_filename == returned_filename
         assert di.get_volumes_from_h5m(test_h5m_filename) == [1]
         assert di.get_materials_from_h5m(test_h5m_filename) == ["mat1"]
-        assert di.get_volumes_and_materials_from_h5m(test_h5m_filename) == {1: "mat1"}
+        assert di.get_volumes_and_materials_from_h5m(
+            test_h5m_filename) == {1: "mat1"}
 
     def test_h5m_file_creation_and_contents_in_subfolder(self):
         """Checks that a h5m file is created in a subfolder from a single stl
@@ -40,7 +41,7 @@ class TestApiUsage(unittest.TestCase):
 
         returned_filename = stl_to_h5m(
             files_with_tags=[
-                    ("tests/part1.stl", "mat1")
+                ("tests/part1.stl", "mat1")
             ],
             h5m_filename=test_h5m_filename,
         )
@@ -50,7 +51,8 @@ class TestApiUsage(unittest.TestCase):
         assert test_h5m_filename == returned_filename
         assert di.get_volumes_from_h5m(test_h5m_filename) == [1]
         assert di.get_materials_from_h5m(test_h5m_filename) == ["mat1"]
-        assert di.get_volumes_and_materials_from_h5m(test_h5m_filename) == {1: "mat1"}
+        assert di.get_volumes_and_materials_from_h5m(
+            test_h5m_filename) == {1: "mat1"}
 
     def test_h5m_file_creation_and_contents_from_multiple_h5m_files(self):
         """Checks that a h5m file is created from multiple stl files"""
@@ -60,8 +62,8 @@ class TestApiUsage(unittest.TestCase):
 
         returned_filename = stl_to_h5m(
             files_with_tags=[
-                    ("tests/part1.stl", "mat1"),
-                    ("tests/part2.stl", "mat2")
+                ("tests/part1.stl", "mat1"),
+                ("tests/part2.stl", "mat2")
             ],
             h5m_filename=test_h5m_filename,
         )
