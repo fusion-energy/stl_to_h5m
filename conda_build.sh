0c134d45f05d9cc8b775fa5689c12a6130c28246
Jonathan <mail@jshimwell.com>
2022-02-07 22:09:49 +0000

    using version in meta yml file

--- conda_build.sh
+++ conda_build.sh
@@ -5,30 +5,12 @@
 # anaconda login
 
 #cleans up files that get uploaded to the anaconda repo if not deleted
-find . -name \*.svg -type f -delete
 find . -name \*.stl -type f -delete
-find . -name \*.stp -type f -delete
-find . -name \*.step -type f -delete
-find . -name \*.brep -type f -delete
-find . -name \*.png -type f -delete
-find . -name \*.gif -type f -delete
-find . -name \*.html -type f -delete
-find . -name \*.json -type f -delete
 
 
 rm -rf /tmp/conda-build
 mkdir -p /tmp/conda-build
 rm -rf /tmp/conda-build
-
-
-# VERSION=$(echo $GITHUB_REF | sed 's#.*/v##')
-VERSION=0.2.0
-PLACEHOLDER='version="develop"'
-VERSION_FILE='setup.py'
-# Grep checks that the placeholder is in the file. If grep doesn't find
-# the placeholder then it exits with exit code 1 and github actions fails.
-grep "$PLACEHOLDER" "$VERSION_FILE"
-sed -i "s@$PLACEHOLDER@version=\"${VERSION}\"@g" "$VERSION_FILE"
 
 conda-build conda/ -c cadquery -c conda-forge --croot /tmp/conda-build 
 
@@ -49,5 +31,3 @@ do
 done
 
 anaconda upload -f /tmp/conda-build/*/*.tar.bz2
-
-sed -i "s@version=\"${VERSION}\"@$PLACEHOLDER@g" "$VERSION_FILE"
